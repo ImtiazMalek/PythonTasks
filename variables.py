11ae943bf681c8c1c3c2352d30e7d59c04f84fe1
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-05 02:44:14 +0600

    declaring variable in python

---
+++ variables.py
@@ -0,0 +1,7 @@
+cha_name = 'imtiaz'
+int_age = 24
+print("there once was a man named "+cha_name+",")
+print("his age was %s"%int_age+".")
+cha_name = 'bushra'
+print("he liked the name "+cha_name+",")
+print("But he didn't like to be %s"%int_age+".")
