39008664c9e43ac144640c983d844f32cb87b7f9
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-10 04:37:30 +0600

    taking input from user

---
+++ input.py
@@ -0,0 +1,3 @@
+name=input("Enter your name: ")#user will inter name and comment
+age=input("Enter your age: ")#and we are saving it under various variables
+print("Hello "+name+", you are "+age+" years old")#it'll print the result
