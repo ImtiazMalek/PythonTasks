e1f0814588dfcf812595cc20c55c1a4a1967fadd
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-10 05:12:13 +0600

    A calculator usning python
    all the results will me till 4th decimal point

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1= input ("enter a number: " )
+num2= input ("enter another number: " )
+#functions
+add= float(num1)+float(num2)
+sub= float(num1)-float(num2)
+mul= float(num1)*float(num2)
+div= float(num1)/float(num2)
+print("addition is: "+"%.4f"%add+"\nsubtruction is: "+"%.4f"%sub+"\nmultiplication is: "+"%.4f"%mul+"\nAnd division is :"+"%.4f"%div)
+#"%.4f"% menas , it will print 4 digit after decimal point
