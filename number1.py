e5abd4802e43e79cd2c88301674b90e292a7e9ec
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-10 04:26:16 +0600

    some examples of using math function in python

---
+++ number1.py
@@ -0,0 +1,10 @@
+from math import * #this'll allow us to use various math function
+my_num = -5.6
+num=3.6
+print(str(my_num)+" is my number")
+print(pow(my_num,2))
+print(floor(num))
+print(abs(my_num))
+print(round(my_num))
+print(ceil(my_num))
+print(sqrt(num))
