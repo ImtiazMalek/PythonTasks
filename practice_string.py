c4a46c7a717125311e5ece9242ec3c607d70830b
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-10 04:01:30 +0600

    example of function and string

---
+++ practice_string.py
@@ -0,0 +1,5 @@
+phrase = "welcome to imtiaz Academy"
+print("Hello \n" + phrase+ ", which is bad ass")
+print("the 5th character is "+ phrase[5])
+print(phrase.replace("imtiaz", "junaid"))
+print(phrase)
