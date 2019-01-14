e7fd204680d220eb8b9720d54f66301c3813e16d
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-15 00:35:54 +0600

    Another example of function and implementation

---
+++ function_example.py
@@ -0,0 +1,6 @@
+user_name = input('Enter your name: ')
+role= input('Enter your post: ')
+def massage(user_name:str,role:str):
+    print(f'Hello {user_name}\nYour post is {role}')
+print ('Welcome')
+massage(user_name,role)
