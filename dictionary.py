945713025e1768b1eec5e05267b393727b4885c8
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-16 03:48:10 +0600

    A dictionary and retrieving data from it

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+#entering players name
+
+month={
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+input('Enter a jersey number '):input('Enter a name'),
+}
+
+#retrieving name by jersey number
+
+enter=input('Enter jersey no. you want to find: ')
+print(month.get(enter,'Not a valid jersey'))
